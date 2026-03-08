9a209006fd89e4ec9ad30f3655f7b0f54d730596
concl <107962474+concl@users.noreply.github.com>
2026-03-08 14:48:03 -0700

    modified moving source for testing

--- Assets/Script/Oscillator.cs
+++ Assets/Script/Oscillator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     public float distance = 5f;
-    public float speed = 2f;
+    public float speed = 200f;
     public Vector3 direction = Vector3.right;
     
     private Vector3 startPosition;
@@ -16,7 +16,13 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
-        float offset = Mathf.Sin(Time.time * speed) * distance;
-        transform.position = startPosition + direction * offset;
+        float offset = speed * Time.deltaTime;
+        transform.position += direction * offset;
+
+        Debug.Log($"Current position: {transform.position}, start position: {startPosition}, distance: {startPosition.x + distance}.");
+        if (transform.position.x > startPosition.x + distance)
+        {
+            transform.position = startPosition;
+        }
     }
 }
